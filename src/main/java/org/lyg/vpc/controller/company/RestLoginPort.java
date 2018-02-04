4fd08e843cbd5ca705929012ae5e2b53e00357cc
yaoguangluo <fengyue1985>
2018-02-04 17:41:23 +0800

    VPC的前端采用html布局， angular做数据分析

---
+++ src/main/java/org/lyg/vpc/controller/company/RestLoginPort.java
@@ -0,0 +1,83 @@
+package org.lyg.vpc.controller.company;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.lyg.common.RequestLimit;
+import java.io.IOException;
+import java.util.Map;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+
+import static org.lyg.common.constants.MiniConstant.DB_LOGIN_U_ADDRESS;
+import static org.lyg.common.constants.MiniConstant.DB_LOGIN_U_AGE;
+import static org.lyg.common.constants.MiniConstant.DB_LOGIN_U_CHANGE;
+import static org.lyg.common.constants.MiniConstant.DB_LOGIN_U_CHANGE_ENSURE;
+import static org.lyg.common.constants.MiniConstant.DB_LOGIN_U_EMAIL;
+import static org.lyg.common.constants.MiniConstant.DB_LOGIN_U_EMAIL_ENSURE;
+import static org.lyg.common.constants.MiniConstant.DB_LOGIN_U_NAME;
+import static org.lyg.common.constants.MiniConstant.DB_LOGIN_U_PHONE;
+import static org.lyg.common.constants.MiniConstant.DB_LOGIN_U_PSTOKEN;
+import static org.lyg.common.constants.MiniConstant.DB_LOGIN_U_PSWD;
+import static org.lyg.common.constants.MiniConstant.DB_LOGIN_U_PSWD_ENSURE;
+import static org.lyg.common.constants.MiniConstant.DB_LOGIN_U_QQ;
+import static org.lyg.common.constants.MiniConstant.DB_LOGIN_U_SEX;
+import static org.lyg.common.constants.MiniConstant.DB_LOGIN_U_WECHAT;
+import static org.lyg.common.constants.MiniConstant.DIGIT_60000;
+import static org.lyg.common.constants.MiniConstant.DIGIT_ONE;
+import static org.lyg.common.constants.MiniConstant.REST_JSON_CONFIG;
+import static org.lyg.common.constants.MiniConstant.REST_LOGIN_CHANGE;
+import static org.lyg.common.constants.MiniConstant.REST_LOGIN_FIND;
+import static org.lyg.common.constants.MiniConstant.REST_LOGIN_LOGIN;
+import static org.lyg.common.constants.MiniConstant.REST_LOGIN_LOGOUT;
+import static org.lyg.common.constants.MiniConstant.REST_LOGIN_REGISTER;
+
+@RestController
+public interface RestLoginPort {
+    @RequestLimit(count = DIGIT_ONE, time = DIGIT_60000)
+    @POST
+    @RequestMapping(REST_LOGIN_LOGIN)
+    @Produces(REST_JSON_CONFIG)
+    Map<String, Object> login(@QueryParam(DB_LOGIN_U_EMAIL) String uEmail,
+                              @QueryParam(DB_LOGIN_U_PSWD) String uPasswd) throws IOException;
+
+    @RequestLimit(count = DIGIT_ONE, time = DIGIT_60000)
+    @GET
+    @RequestMapping(REST_LOGIN_LOGOUT)
+    @Produces(REST_JSON_CONFIG)
+    Map<String, Object> logout(@QueryParam(DB_LOGIN_U_EMAIL) String uEmail,
+                               @QueryParam(DB_LOGIN_U_PSTOKEN) String uPSToken) throws IOException;
+
+    @RequestLimit(count = DIGIT_ONE, time = DIGIT_60000)
+    @POST
+    @RequestMapping(REST_LOGIN_REGISTER)
+    @Produces(REST_JSON_CONFIG)
+    Map<String, Object> register(@QueryParam(DB_LOGIN_U_EMAIL) String uEmail,
+                                 @QueryParam(DB_LOGIN_U_EMAIL_ENSURE) String uEmailEnsure,
+                                 @QueryParam(DB_LOGIN_U_NAME) String uName,
+                                 @QueryParam(DB_LOGIN_U_PSWD) String uPassWD,
+                                 @QueryParam(DB_LOGIN_U_PSWD_ENSURE) String uPassWDEnsure,
+                                 @QueryParam(DB_LOGIN_U_ADDRESS) String uAddress,
+                                 @QueryParam(DB_LOGIN_U_PHONE) String uPhone,
+                                 @QueryParam(DB_LOGIN_U_WECHAT) String uWeChat,
+                                 @QueryParam(DB_LOGIN_U_QQ) String uQq,
+                                 @QueryParam(DB_LOGIN_U_AGE) String uAge,
+                                 @QueryParam(DB_LOGIN_U_SEX) String uSex) throws IOException;
+
+    @RequestLimit(count = DIGIT_ONE, time = DIGIT_60000)
+    @POST
+    @RequestMapping(REST_LOGIN_CHANGE)
+    @Produces(REST_JSON_CONFIG)
+    Map<String, Object> change(@QueryParam(DB_LOGIN_U_EMAIL) String uEmail,
+                               @QueryParam(DB_LOGIN_U_CHANGE) String uChange,
+                               @QueryParam(DB_LOGIN_U_CHANGE_ENSURE) String uChangeEnsure,
+                               @QueryParam(DB_LOGIN_U_PSTOKEN) String uPSToken,
+                               @QueryParam(DB_LOGIN_U_PSWD) String uPassWD) throws IOException;
+
+    @RequestLimit(count = DIGIT_ONE, time = DIGIT_60000)
+    @POST
+    @RequestMapping(REST_LOGIN_FIND)
+    @Produces(REST_JSON_CONFIG)
+    Map<String, Object> find(@QueryParam(DB_LOGIN_U_EMAIL) String uEmail) throws IOException;
+}
